da7d8920e6fdf407e72d0bf9427dabdf1d94b3c9
Daniel J. Dean <dade@microsoft.com>
2022-07-06 11:42:11 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 const yaml = require('js-yaml');
-try {
-  const configData = core.getInput('config');
-  fs.readFile(configData, 'utf8', (err, data) => {
+try {  
+    const configData = core.getInput('config');
+    fs.readFile(configData, 'utf8', (err, data) => {
     if (err) {
       console.error(err);
       return;
     }
-  const configYaml = yaml.load(configData, 'utf8');
-  console.log(configYaml);
-  core.setOutput("test", configYaml["variables"]["resource_group"]);
+    console.log(data);
+    const configYaml = yaml.load(data, 'utf8');
+    core.setOutput("test", configYaml["variables"]["resource_group"]);
+  });
+  
 } catch (error) {
   core.setFailed(error.message);
 }
